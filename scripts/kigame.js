cc88452be487944eb09ba47c3b56545282c10933
kigamekisia <kigamekisia@gmail.com>
2022-09-17 20:15:11 +0300

    footer update

--- scripts/kigame.js
+++ scripts/kigame.js
@@ -8,3 +8,8 @@ function photoDisplay(){
   document.getElementById("photo").setAttribute("src", images.kigame,); 
  };
 photoDisplay()
+let d = new Date;
+let dayName = daynames[d.getDay()];
+let monthName= months[d.getMonth()];
+year = d.getFullYear();
+document.getElementById("date").textContent = year;
