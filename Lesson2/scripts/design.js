5cb159fbf903b457426fe6376a81c20dd2effc45
Kigame <97285327+kigamekisia@users.noreply.github.com>
2022-09-25 16:01:15 +0300

    delete

--- Lesson2/scripts/design.js
+++
@@ -1,5 +0,0 @@
-let d = new Date();
-let year = d.getFullYear();
-document.querySelector("#currentyear").innerHTML = year;
-let lastModified = document.lastModified;
-document.getElementById("modified").innerHTML = lastModified;
