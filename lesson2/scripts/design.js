330dfce76a3949646a961be921390c4c6561242b
kigamekisia <kigamekisia@gmail.com>
2022-09-26 10:48:57 +0300

    Update

--- lesson2/scripts/design.js
+++ lesson2/scripts/design.js
@@ -1,3 +1,15 @@
+let images = {
+    quip: "images/quip_image.jpg",
+    momentum: "images/momentum_image.jpg",
+    squarespace: "images/squarespace_image.jpg",
+  };
+  
+  function photoDisplay(){
+    document.getElementById("first").setAttribute("src", images.quip,); 
+    document.getElementById("second").setAttribute("src", images.momentum,); 
+    document.getElementById("third").setAttribute("src", images.squarespace,); 
+   };
+  photoDisplay()
 let d = new Date();
 let year = d.getFullYear();
 document.querySelector("#year").innerHTML = year;
