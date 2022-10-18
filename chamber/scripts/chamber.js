3e347d8d6e34ab420838c11c22382dd6987aaf24
kigamekisia <kigamekisia@gmail.com>
2022-10-18 10:32:05 +0300

    update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -6,3 +6,8 @@ d = new Date();
 let day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][d.getDay()];
 let month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "November", "September", "October", "November", "December"][d.getMonth()]
 document.getElementById("date").textContent = day + ", " + String(d.getDate()) + " " + month + " " + d.getFullYear();
+
+// banner
+if(d.getDay() ==1 || d.getDay() == 2){
+    document.getElementById("banner").style.display="block";
+} 
