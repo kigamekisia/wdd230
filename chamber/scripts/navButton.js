330fac38c50dbf81d95c1bddd5a03be68adc873a
kigamekisia <kigamekisia@gmail.com>
2022-10-27 11:45:42 +0300

    update

--- chamber/scripts/navButton.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    document.getElementById("navigate").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-  }
-  
-  const x = document.getElementById("hamburgerBtn");
-  x.onclick = toggleMenu;
