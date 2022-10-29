394a2c53cee5a4b3738c12613e0603170ef56e77
kigamekisia <kigamekisia@gmail.com>
2022-10-29 08:18:46 +0300

    update

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,3 +1,18 @@
+// let temperature = parseFloat(document.getElementById("temp").innerText);
+// let windSpeed = parseFloat(document.getElementById("wind").innerText);
+// let windChill = "";
+
+
+
+// if ((temp <= 50) && (wind > 3.0)) {
+//     windChill = 35.74 + (0.6215 * temperature) - 
+//     (35.75 * (windSpeed ** 0.16)) + (0.4275 * temperature * (windSpeed ** 0.16)); 
+//     windChill = Math.round(windChill);
+// } else {
+//     windChill = "N/A";
+// }
+// document.querySelector("#chill").innerHTML = windChill;
+
 let tempElement = document.getElementById("temp");
 let windElement = document.getElementById("wind");
 let chillElement = document.getElementById("chill");
