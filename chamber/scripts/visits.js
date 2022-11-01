760a5124a13cc023099d7ebf7b3596b99ed6384f
kigamekisia <kigamekisia@gmail.com>
2022-11-01 09:20:41 +0300

    update

--- chamber/scripts/visits.js
+++ chamber/scripts/visits.js
@@ -1,9 +1,9 @@
 // initialize display elements
-const todayDisplay = document.querySelector("#lastvisit");
+const lastVisitDisplay = document.querySelector("#lastvisit");
 const visitsDisplay = document.querySelector("#visits");
 
 // get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("lastvisit"));
+let numVisits = Number(window.localStorage.getItem("visits"));
 
 // determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
@@ -15,4 +15,13 @@ if (numVisits !== 0) {
 // increment the number of visits.
 numVisits++;
 // store the new number of visits value
-localStorage.setItem("lastvisit", numVisits);
+localStorage.setItem("visits", numVisits);
+
+lastvisit = localStorage.getItem("lastvist")
+
+if (lastvisit == null) {
+    lastvisit = new Date()
+} 
+
+lastVisitDisplay.textContent = Math.floor((new Date() - lastvisit) / (1000 * 60 * 60 * 24));
+localStorage.setItem("lastvisit", new Date())
